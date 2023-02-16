ee731495441fd4e6e0b9a840e30b7cd312a30ee6
ykcab <ambuku@oreilly.com>
2023-02-15 19:07:53 -0500

    test ping internal

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
 import hvac
 
 # client = hvac.Client(url='https://itops-playground-vault-01.corp.oreilly.com:8200')
-client = hvac.Client(url='https://10.200.80.2:8200',verify=False)
+client = hvac.Client(url='https://itops-playground-vault-01:8200', verify=False)
+
 secret = client.secrets.gcp.generate_service_account_key(roleset="",key_type='TYPE_GOOGLE_CREDENTIALS_FILE', mount_point='gcp/static-account/sa_token')
 
 # Print the value of the secret
