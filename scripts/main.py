f5cfad83873e992d4d542a5c84f9f1a8706ce460
ykcab <ambuku@oreilly.com>
2023-02-28 16:11:15 -0500

    fixed path

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,10 @@
 import hvac
 import os
-# client = hvac.Client(url='https://itops-playground-vault-01.corp.oreilly.com:8200')
-vault_token=os.environ.get("VAULT_TOKEN")
+
+vault_token=os.environ.get("vault_token")
 client = hvac.Client(url='https://10.200.80.2:8200', token= vault_token, verify=False, )
 
-secret = client.secrets.gcp.generate_service_account_key(roleset="",key_type='TYPE_GOOGLE_CREDENTIALS_FILE', mount_point='gcp/static-account/sa_token')
-
+# secret = client.secrets.gcp.generate_service_account_key(roleset="",key_type='TYPE_GOOGLE_CREDENTIALS_FILE', mount_point='gcp/static-account/sa_token')
+secret= client.secrets.kv.v1.read_secret(mount_point='secret/infractl/google_application_credentials/itops-playground')
 # Print the value of the secret
 print(secret)
