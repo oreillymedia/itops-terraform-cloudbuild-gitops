d39d6ae5dfb4521126e08cf14e5e016c6254cffa
ykcab <ambuku@oreilly.com>
2023-02-28 14:47:15 -0500

    testing with a vault key

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,8 @@
 import hvac
-
+import os
 # client = hvac.Client(url='https://itops-playground-vault-01.corp.oreilly.com:8200')
-client = hvac.Client(url='https://10.200.80.2:8200', verify=False)
+vault_token=os.environ.get("VAULT_TOKEN")
+client = hvac.Client(url='https://10.200.80.2:8200', token= vault_token, verify=False, )
 
 secret = client.secrets.gcp.generate_service_account_key(roleset="",key_type='TYPE_GOOGLE_CREDENTIALS_FILE', mount_point='gcp/static-account/sa_token')
 
