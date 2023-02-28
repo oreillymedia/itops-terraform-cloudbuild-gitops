d0b9352999fb341fbe07e7ff3427efeb9a3fb2d2
ykcab <ambuku@oreilly.com>
2023-02-28 16:54:24 -0500

    read with uppercase var name

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 import hvac
 import os
 
-vault_token=os.environ.get("vault_token")
+vault_token=os.environ.get("VAULT_TOKEN")
 client = hvac.Client(url='https://10.200.80.2:8200', token= vault_token, verify=False, )
 
 # secret = client.secrets.gcp.generate_service_account_key(roleset="",key_type='TYPE_GOOGLE_CREDENTIALS_FILE', mount_point='gcp/static-account/sa_token')
